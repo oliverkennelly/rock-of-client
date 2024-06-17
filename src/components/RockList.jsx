7e710da6d6e10c5cdf2151b75ef6c871ebb0cf22
Oliver Kennelly <victoriakennelly@gmail.com>
2024-06-17 10:23:31 -0500

    removed bug and added rock ownership view

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -8,6 +8,7 @@ export const RockList = ({ rocks, fetchRocks }) => {
         if (rocks && rocks.length) {
             return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
                 {rock.name} ({rock.type.label})
+                <div>In the collection of {rock.user?.first_name} {rock.user?.last_name}</div>
             </div>)
         }
 
